9e372ed365629b08511775c36608374b8fd3042f
Joshua Wang <wangjoshua8@gmail.com>
2024-04-02 14:18:35 +1000

    Fix adjacency matrix and algorithm; rename functions.

--- pagerank_centrality.py
+++ pagerank_centrality.py
@@ -2,11 +2,10 @@
 Code for calculating PageRank Centrality for social network nodes.
 """
 
-from networkx import adjacency_matrix
 import numpy as np
 
 
-def find_pagerank_centralities(graph):
+def find_pagerank(graph):
     """
     Calculates the PageRank centrality for all nodes in a graph.
 
@@ -20,25 +19,33 @@ def find_pagerank_centralities(graph):
     beta = 0.15
 
     # Build adjacency matrix
-    A = adjacency_matrix(graph).toarray()
+    n = len(graph.nodes())
+    A = np.zeros((n, n))
+
+    # Populate the adjacency matrix
+    for i in range(n):
+        neighbor_list = list(graph.neighbors(i))
+        for j in range(n):
+            if j in neighbor_list:
+                A[i][j] = 1
 
     # Build inverse degree matrix
-    D_inv = np.diag([1 / len(graph[i]) for i in graph.nodes])
+    D_inv = np.linalg.inv(np.diag(A.sum(axis=1)))
     
-    prev_pagerank_vector = np.zeros(len(graph.nodes()))
-    pagerank_vector = np.ones(len(graph.nodes())) / len(graph.nodes())
+    prev_pagerank_vector = np.zeros(n)
+    pagerank_vector = np.ones(n) / n
 
-    while np.sum(np.abs(prev_pagerank_vector - pagerank_vector)) > 0.0001:
-        prev_pagerank_vector = pagerank_vector.copy()
+    while np.sum(np.abs(prev_pagerank_vector - pagerank_vector)) > 0.001:
+        prev_pagerank_vector = pagerank_vector
 
         # Perform PageRank calculation
-        pagerank_vector = np.matmul(np.matmul(alpha * A.T, D_inv), pagerank_vector) + beta * np.ones(len(graph.nodes()))
+        pagerank_vector = np.matmul(np.matmul(alpha * A.T, D_inv), pagerank_vector) + beta * np.ones(n)
         pagerank_vector = pagerank_vector / np.sum(np.abs(pagerank_vector))
 
     return pagerank_vector
 
 
-def find_top_pagerank_nodes(pagerank_centralities, num):
+def top_pagerank(pagerank_centralities, num):
     """
     Finds the nodes with the greatest pagerank centralities.
 
